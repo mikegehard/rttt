63bb16c1163bad5387b23c1b3ac88658665434de
fjasonvasquez <fjvasquez@aol.com>
2015-10-22 18:39:22 -0700

    rttt built

--- lib/rttt.rb
+++ lib/rttt.rb
@@ -21,10 +21,44 @@ class TicTacToe
 		combinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 		for combination in combinations
 			if (@board[combination[0]].eql?(@board[combination[1]]) && @board[combination[0]].eql?(@board[combination[2]]) && @board[combination[0]] != nil )
-				return (@turn ? true : false)
+				return (@turn ? "X" : "O")
 			end
 		end
 		return nil
+	end
+
+	def draw?
+		count = 0
+		for tile in @board
+			if (!tile.eql?(nil))
+				count = count + 1
+			end
+		end
+
+		if (has_winner? == false)
+			if(count == board.length)
+				return true
+			else
+				return false
+			end
+		else
+			return false
+		end
+	end
+
+	def has_winner?
+		!!winner?
+	end
+
+	def reset!
+		(0...board.length).each do |i|
+			@board[i] = nil
+	end
+
+	def over?
+		has_winner? || draw?
+	end
+
 	end
 
 end
--- spec/lib/rttt_spec.rb
+++ spec/lib/rttt_spec.rb
@@ -18,6 +18,41 @@ describe TicTacToe do
 						'O','X','O',
 						'O','O','X']
 		game.set_board(board)
-		expect(game.winner?).to eq(true)
+		expect(game.winner?).to eq('X')
 	end
+
+	it "is a draw" do
+		board = ['X','O','X',
+						'X','O','X',
+						'O','X','O']
+		game.set_board(board)
+		expect(game.draw?).to eq(true)
+	end
+
+	it "can reset the game" do
+		board = ['X','O','X',
+						'X','O','X',
+						'O','X','O']
+		game.set_board(board)
+		game.reset!
+		expect(game.board[0]).to eq(nil)
+	end
+
+	it "knows when the game is over" do 
+		board = ['X','O','X',
+						'X','O','X',
+						'O','X','O']
+		game.set_board(board)
+		expect(game.over?).to eq(true)
+	end
+
+	it "lets you know who the winner is" do
+		board = ['X','X','X',
+						'X','O','X',
+						'O','X','O']
+		game.set_board(board)
+		game.has_winner?
+		expect(game.winner?).to eq('X')
+	end
+
 end
