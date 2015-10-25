c22562892964fb3327b8e6fe08c57054d8ff254c
fjasonvasquez <fjvasquez@aol.com>
2015-10-25 14:40:08 -0700

    who goes first and turns

--- spec/GameSpec.rb
+++ spec/GameSpec.rb
@@ -16,4 +16,15 @@ describe Game do
 	it "can display a welcome message" do
 		expect(game.instructions).to eq("Welcome to Ruby Tic Tac Toe!\nThe board is laid out as follows.\n\n 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9 \n\n Press Enter to continue..")
 	end
+
+	it "can return a board as a string" do
+		game.tictactoe.stub(:board).and_return(Array.new(9, " "))
+		expect(game.display_board. == ("\n  |  |  \n  |  |  \n  |  |  "))
+	end
+
+	it "can ask who will go first" do
+		game.stub(:gets).and_return("1")
+		game.pick_first
+		expect(game.turn).to eq(true)
+	end
 end
